7ab2beba2cc7c2822582a388a12951221d619d8c
QB-KY <68432050+QB-KY@users.noreply.github.com>
2020-09-30 14:12:27 +0800

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,11 +1,19 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-     int i;
-     int j;
-     int k;
-     for( i=-3; i<=3; i++ )
-     {
-         for( j=
-     }
+    int i;
+    int j;
+    int k;
+    
+    for( i=-3; i<=3; i++ )
+    {
+        for( j=1; j<=abs(i); j++)
+            printf(" ");
+          
+        for( k=abs(i)-3; k<=3-abs(i); k++)
+            printf("%c",64+abs(j)+abs(k));
+        printf("\n");
+    }
+    
 }
