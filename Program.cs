258070c51f2962011e70fb2c97c39672910b4f36
QB-KY <68432050+QB-KY@users.noreply.github.com>
2021-07-25 19:44:30 +0800

    added a file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入班级：");
+            String strclass = Console.ReadLine();
+            Console.WriteLine("请输入学号：");
+            String number = Console.ReadLine();
+            Console.WriteLine("请输入姓名：");
+            String name = Console.ReadLine();
+            Console.WriteLine("你是{0}班的{1}号{2}",strclass,number,name);
+        }
+    }
+}
